6a330f30ea19de549e6992aa7c898f8b5bf124a1
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-15 17:26:50 +1100

    Part 1

--- level-one.js
+++ level-one.js
@@ -2,6 +2,7 @@
  * DOTS: Level One
  *
  */
+const body = document.querySelector('body');
 
 const jsball = document.querySelector('.js-ball');
 const score = document.querySelector('.js-score');
@@ -10,20 +11,21 @@ const textWin = document.querySelector('.--xl-large')
 const linkWin = document.querySelector('.level-winner > p')
 const lv2 = document.querySelector('.level-winner > p > a')
 
-console.log(lv3)
+
 
 jsball.addEventListener('click', function() {
     let num = 0;
-    num = parseInt(score.textContent) + 10;
+    num = parseInt(score.textContent) + 50;
     score.textContent = '0000'.substring(String(num).length) + num;
     console.log(parseInt(score.textContent));
 
     if (parseInt(score.textContent) >= 100) {
-        winner.style.opacity = '1';
-        winner.style.backgroundColor = 'white';
-        textWin.style.color = 'black';
-        linkWin.style.color = 'black';
-        lv3.style.color = 'blue';
+        body.className = 'game-over'
+        // winner.style.opacity = '1';
+        // winner.style.backgroundColor = 'white';
+        // textWin.style.color = 'black';
+        // linkWin.style.color = 'black';
+        // lv2.style.color = 'blue';
     }
 })
 
