bfa46cc900a733c7e0c84df1e896654821f96d41
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-15 16:22:19 +1100

    Level 3

--- level-three.js
+++ level-three.js
@@ -2,3 +2,36 @@
  * DOTS: Level Three
  *
  */
+
+/*
+ * DOTS: Level Two
+ *
+ */
+
+const jsballs = document.querySelectorAll('.js-ball');
+const score = document.querySelector('.js-score');
+
+const winner = document.querySelector('.level-winner');
+const textWin = document.querySelector('.--xl-large')
+const linkWin = document.querySelector('.level-winner > p')
+const lv3 = document.querySelector('.level-winner > p > a')
+
+
+console.log(jsballs[1].dataset.increment)
+
+for (let i = 0; i < jsballs.length; i++) {
+    jsballs[i].addEventListener('click', function() {
+        let num = 0;
+        num = parseInt(score.textContent) + parseInt(jsballs[i].dataset.increment);
+        score.textContent = '0000'.substring(String(num).length) + num;
+        console.log(parseInt(score.textContent));
+    
+        if (parseInt(score.textContent) >= 100) {
+            winner.style.opacity = '1';
+            winner.style.backgroundColor = 'white';
+            textWin.style.color = 'black';
+            linkWin.style.color = 'black';
+            lv3.style.color = 'blue';
+        }
+    })
+}
